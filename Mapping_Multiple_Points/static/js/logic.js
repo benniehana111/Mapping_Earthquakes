4cdf4238139ee5018c490623899032e5f6695d0b
benniehana111 <103535389+benniehana111@users.noreply.github.com>
2022-09-25 20:35:51 -0400

    Add files via upload

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -1,5 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
+
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
@@ -23,15 +24,12 @@ let cityData = cities;
  .addTo(map)
 });
 
-
 //Street Map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
    maxZoom: 4,
   accessToken: API_KEY
 });
-
-
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
